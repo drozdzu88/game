2b52712d1213c56a7c6bec323ace76562c6a8123
drozdzu88 <drozdzu88@gmail.com>
2024-04-23 13:37:24 +0200

    character build change

--- src/main.rs
+++ src/main.rs
@@ -5,6 +5,8 @@ use lib::character::Class;
 fn main() {
     // let char_1 = create_new_character(String::from("Łukasz"), Class::Mage);
     let char_1 = lib::character::Character::create_new_character(String::from("Łukasz"), Class::Mage);
+    let char_2 = lib::character::Character::create_new_character(String::from("Łukasz"), Class::Palladin);
 
     char_1.describe();
+    char_2.describe();
 }
